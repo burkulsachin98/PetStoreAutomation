5aad7454b79fc4f7670ecf8d674c2e36ef7a2422
burkulsachin <sachinburkul2006@gmail.com>
2024-12-16 12:13:01 +0530

    Dataproviders Commit

--- src/test/java/api/test/UserTests.java
+++ src/test/java/api/test/UserTests.java
@@ -31,39 +31,39 @@ public class UserTests {
 	}
 	@Test(priority=1)
 	public void  testPostUser() {
-		
+
 		Response res =UserEndPoints.CreateUser(userPayload);
 		res.then().log().all();
 		Assert.assertEquals(res.getStatusCode(),200);
-		
+
 	}
 	@Test(priority=2)
 	public void getUserByName() {
-		
+
 		Response res =UserEndPoints.ReadUser(this.userPayload.getUsername());
-		
+
 		res.then().log().all();
 		Assert.assertEquals(res.getStatusCode(), 200);	
 	}	
 	public void updateUser() {
-		
+
 		userPayload.setFirstname(faker.name().firstName());
 		userPayload.setLastname(faker.name().lastName());
 		userPayload.setEmail(faker.internet().emailAddress());
 		Response response=UserEndPoints.UpdateUser(this.userPayload.getUsername(),userPayload);
 		response.then().log().all();
 		Assert.assertEquals(response.getStatusCode(), 200);
-		
+
 		//Check response after update 
 		Response responseAfterUpdate=UserEndPoints.ReadUser(this.userPayload.getUsername());
 		Assert.assertEquals(responseAfterUpdate.getStatusCode(), 200);
-		
+
 	}
-	
+
 	public void testDeleteUserByName() {
 		Response  response =UserEndPoints.DeleteUser(this.userPayload.getUsername());
 		Assert.assertEquals(response.statusCode(), 200);
-		
-		
+
+
 	}
 }
